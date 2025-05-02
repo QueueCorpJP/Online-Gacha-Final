2a8fef6121064edb3b2b8b8a6ea76832925f7338
boby john <user@example.com>
2025-05-02 17:03:57 +0900

    ポイントページでコインの残高を正しく取得できるよう修正

--- frontend/app/profile/points/client.tsx
+++ frontend/app/profile/points/client.tsx
@@ -1,12 +1,22 @@
 "use client"
 
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { AppDispatch } from "@/redux/store"
 import { useTranslations } from "@/hooks/use-translations"
 import { PointsBalance } from "@/components/profile/points/points-balance"
 import { PointsHistory } from "@/components/profile/points/points-history"
 import { ProfileTabs } from "@/components/profile/profile-tabs"
+import { fetchProfile } from "@/redux/features/profileSlice"
 
 export default function PointsClient() {
   const { t } = useTranslations()
+  const dispatch = useDispatch<AppDispatch>()
+  
+  useEffect(() => {
+    // ポイントページマウント時にプロフィール情報を取得
+    dispatch(fetchProfile())
+  }, [dispatch])
 
   return (
     <div className="bg-[#F8FAFC] xl:px-20 xl:py-8">
@@ -14,7 +24,7 @@ export default function PointsClient() {
       <ProfileTabs />
       <div className="container-fluid py-6 px-4 lg:px-0">
         <div className="rounded-lg border bg-white p-6 mb-6">
-          <PointsBalance points={1234} />
+          <PointsBalance />
         </div>
         <div className="rounded-lg border bg-white p-6">
           <PointsHistory />
