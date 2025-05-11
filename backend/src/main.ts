4df15e86c4589ed2a73d1b6c50741df31a1d081a
sekiguchimend <sekiguchishunya0619@gmail.com>
2025-05-12 00:03:15 +0900

    build

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -61,8 +61,7 @@ async function bootstrap() {
   //   .build();
 
   // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api/docs', app, document);
-
+  // SwaggerModule.setup('api/docs', app, document
   await app.listen(3001);
 }
 bootstrap();
