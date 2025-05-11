2822bac6972f3d8afeb28697d60c1b59ace733c2
sekiguchimend <sekiguchishunya0619@gmail.com>
2025-05-12 05:11:17 +0900

    修正2

--- backend/src/modules/payments/payment.service.ts
+++ backend/src/modules/payments/payment.service.ts
@@ -30,7 +30,7 @@ export class PaymentService {
     private referralService: ReferralService,
   ) {
     this.stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'), {
-      apiVersion: '2025-01-27.acacia',
+      apiVersion: '2025-01-27.acacia'as any,
       // apiVersion: '2025-02-24.acacia',
     });
     
